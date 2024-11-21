cddab7c89bd0226a8c6fc8eb03e76e3816a43eb1
avk0320 <alexandra.valle020@gmail.com>
2024-11-20 22:42:46 -0700

    added some files

--- online-bookstore/online-bookstore/server/server.js
+++ online-bookstore/online-bookstore/server/server.js
@@ -7,6 +7,21 @@ app.use(express.json());
 app.use('/api/books', booksRoutes);
 app.use('/api/search', searchRoutes);
 
+app.get('/books/:id', (req, res) => {
+    const bookId = req.params.id;
+    const books = [
+        { id: 1, title: 'Book 1', author: 'Author 1', genre: 'Fiction', price: 10 },
+        { id: 2, title: 'Book 2', author: 'Author 2', genre: 'Non-fiction', price: 15 },
+    ];
+    const book = books.find(b => b.id == bookId);
+
+    if (book) {
+        res.render('bookDetails', { book });
+    } else {
+        res.status(404).send('Book not found');
+    }
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
